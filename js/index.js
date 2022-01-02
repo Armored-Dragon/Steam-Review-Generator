7be23616e1b7974ce0fcf9ea26631eb9ff06881e
Armored Dragon <43324896+Armored-Dragon@users.noreply.github.com>
2022-01-02 04:11:04 -0600

    Initial Commit
    v1.0.0

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const qs = (search) => document.querySelector(search);
+
+qs('#recommend').addEventListener('click', () => changeRecommendState(true));
+qs('#not-recommended').addEventListener('click', () => changeRecommendState(false));
+qs('#hours-played-input').addEventListener('keyup', (element) => updateData('hoursplayed', element));
+qs('#date-posted-input').addEventListener('change', (element) => updateData('date', element));
+qs('#review-input').addEventListener('keyup', (element) => updateData('review', element));
+
+function changeRecommendState(state) {
+	if (state) {
+		qs('#review-icon').src = '/img/thumbsup.png';
+		qs('#review-title').innerText = 'Recommended';
+	} else {
+		qs('#review-icon').src = '/img/thumbsdown.png';
+		qs('#review-title').innerText = 'Not Recommended';
+	}
+}
+
+function updateData(data_id, data) {
+	// Date check
+	if (data.target.id === 'date-posted-input') {
+		const month = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+		const date_data = {
+			month: month[Number(data.target.value.split('-')[1]) - 1],
+			day: data.target.value.split('-')[2],
+			year: data.target.value.split('-')[0]
+		};
+		qs(`#${data_id}`).innerText = `${date_data.month} ${date_data.day}, ${date_data.year}`.toUpperCase();
+		return;
+	}
+
+	// Not a date (I know the feel)
+	console.log(data.target.value.toLocaleString().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+	qs(`#${data_id}`).innerText = data.target.value.toLocaleString().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
